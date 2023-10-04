611ae1bf90e551de062d51cb70482673eb124616
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-04 23:09:55 +0530

    updates

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const mongoose = require("mongoose");
 
 const main = async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
-    
+
     //validation
     const product_Schema = new mongoose.Schema({
         //whatever add here that will be add only with same type
-        name:String,  
-        price:Number
+        name: String,
+        price: Number,
+        brand: String,
+        category: String
     });
 
     const productsModel = mongoose.model('products', product_Schema); //model connect node.js to mongoDB
-    const data = new productsModel({name: "Sam", price: 400});
+    const data = new productsModel({ name: "rony", price: 90, brand: "maxx", category: "HOME" });
     const result = await data.save();
     console.log(result);
 
